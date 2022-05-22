f411cc82394039f2ccc0ae26ee29d51e6b80e881
Büşra ÇİÇEK <37375743+bussracicekk@users.noreply.github.com>
2022-05-22 20:14:20 +0300

    Add files via upload

---
+++ PatchRequest/ControllerModels/MovieController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.AspNetCore.Mvc;
+using PatchRequest.Model;
+using Microsoft.AspNetCore.Mvc.NewtonsoftJson;
+
+namespace PatchRequest.ControllerModels
+{
+    [Route("api/movie")]
+    public class MovieController : Controller
+    {
+        IList<Movie> Movies { get; set; }
+
+        public MovieController()
+        {
+            Movies = new List<Movie>();
+
+            Movies.Add(new Movie(1, "A Beautiful Mind","Ron Howard", new System.DateTime(2002, 3, 8)));
+            Movies.Add(new Movie(2, "V For Vandetta", "James McTeigue", new System.DateTime(2006, 3, 31)));
+        }
+
+        [HttpPatch("{id:int}")]
+        public IActionResult Patch(int id, [FromBody] JsonPatchDocument<Movie> patchEntity)
+        {
+            var entity = Movies.FirstOrDefault(movie => movie.Id == id);
+
+            if (entity == null)
+            {
+                return NotFound();
+            }
+
+            patchEntity.ApplyTo(entity, ModelState); // Must have Microsoft.AspNetCore.Mvc.NewtonsoftJson installed
+
+            return Ok(entity);
+        }
+        [HttpGet("{id:int}")]
+        public IActionResult Get(int id)
+        {
+            var entity = Movies.FirstOrDefault(movie => movie.Id == id);
+
+            if (entity == null)
+            {
+                return NotFound();
+            }
+
+
+            return Ok(entity);
+        }
+
+
+    }
+}
---
+++ PatchRequest/Model/Movie.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+
+namespace PatchRequest.Model
+{
+   
+    [JsonObject(MemberSerialization.OptIn)]
+    public partial class Movie
+    {
+        [JsonProperty(PropertyName = "id")]
+        public virtual int Id { get; set; }
+
+        [JsonProperty(PropertyName = "name")]
+        public virtual string Name { get; set; }
+
+        [JsonProperty(PropertyName = "director")]
+        public virtual string Director { get; set; }
+
+        [JsonProperty(PropertyName = "releasedate")]
+        public virtual DateTime? ReleaseDate { get; set; }
+
+        public Movie(int id, string title, string director, DateTime? releaseDate)
+        {
+            Id = id;
+            Name = title;
+            Director = director;
+            ReleaseDate = releaseDate;
+        }
+    }
+}
